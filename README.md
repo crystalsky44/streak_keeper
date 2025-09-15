4d838dec98b4da5994b5b3898a9965223c8a7931
Yoshishige Unose <966.krr@gmail.com>
2025-09-15 17:22:51 +0900

    add: prints a cli input

---
+++ README.md
@@ -0,0 +1,1 @@
+My First Ever Software\nGoal: To keep track of tasks from CLI
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Please input your task: ");
+
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input)
+        .expect("Task title is required to add task");
+
+    println!("Task added: {}", input.trim());
 }
+
+/*
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn input_test() {
+        let task_title = 
+*/
